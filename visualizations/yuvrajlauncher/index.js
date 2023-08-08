cd6ac0dd6c0601d1ca2a4a0c13a2eb71ab5b6180
YUVRAJ KUMAR ARORA <yuvrajarora070@gmail.com>
2023-08-08 12:32:36 +0530

    commit

---
+++ visualizations/yuvrajlauncher/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chart1 from './ychart1'
+import Chart2 from './ychart2';
+import Chart3 from './ychart3';
+
+export default class Yuvrajvzrv extends React.Component {
+    render() {
+
+        return <div>
+            <Chart1/>
+            <Chart2/>
+            <Chart3/>
+        </div>;
+    }
+}
